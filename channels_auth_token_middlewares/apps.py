d41fa8495b66f95047ee65348b3e9a532d61edd3
Yegor Bitensky <yegorbitensky@gmail.com>
2022-01-09 21:42:50 +0300

    Initial

---
+++ channels_auth_token_middlewares/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class ChannelsAuthTokenMiddlewaresConfig(AppConfig):
+    name = 'channels_auth_token_middlewares'
+    verbose_name = 'Channels auth token middlewares'
