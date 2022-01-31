e3d38a1a293d3c10ec4d91f2cae7d59becff1223
Yegor Bitensky <yegorbitensky@gmail.com>
2022-01-31 23:49:10 +0300

    Add cookie auth token middleware

--- channels_auth_token_middlewares/middleware.py
+++ channels_auth_token_middlewares/middleware.py
@@ -1,5 +1,6 @@
 import re
 
+from http.cookies import BaseCookie
 from urllib.parse import parse_qs
 
 from django.apps import apps
@@ -81,6 +82,29 @@ class HeaderAuthTokenMiddleware(BaseAuthTokenMiddleware):
         key = headers.get(self.header_name, b"").decode()
 
         matched = re.fullmatch(rf"{self.keyword} ({self.token_regex})", key)
+        if not matched:
+            return None
+        return matched.group(1)
+
+
+class CookieAuthTokenMiddleware(BaseAuthTokenMiddleware):
+    """Base middleware which parses token key from request cookie."""
+
+    cookie_name = None
+
+    def __init__(self, *args, cookie_name=None, **kwargs):
+        self.cookie_name = str(cookie_name or self.cookie_name)
+
+        super().__init__(*args, **kwargs)
+
+    def parse_token_key(self, scope):
+        headers = dict(scope["headers"])
+        cookie_raw_data = headers.get(b"cookie", b"").decode()
+        cookie = BaseCookie()
+        cookie.load(cookie_raw_data)
+        key = cookie.get(self.cookie_name, '')
+
+        matched = re.fullmatch(rf"({self.token_regex})", key)
         if not matched:
             return None
         return matched.group(1)
