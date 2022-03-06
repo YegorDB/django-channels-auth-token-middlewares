568ed71034cdafc20c6a18bce75e8a4261b37dd9
Yegor Bitensky <yegorbitensky@gmail.com>
2022-03-06 23:00:51 +0300

    Add mock consumer

--- tests/app/tests_app/tests.py
+++ tests/app/tests_app/tests.py
@@ -1,8 +1,6 @@
 from django.test import TestCase
 
-from channels.testing import ApplicationCommunicator
-
-from test_app.consumer import TestHttpConsumer
+from test_app.consumer import MockConsumer
 from test_app.middleware import (
     TestBaseAuthTokenMiddleware, TestHeaderAuthTokenMiddleware,
     TestCookieAuthTokenMiddleware, TestQueryStringAuthTokenMiddleware,
