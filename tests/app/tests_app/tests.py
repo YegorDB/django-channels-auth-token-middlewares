a178c0fe2a8dcabccded310e577ddab153538e47
Yegor Bitensky <yegorbitensky@gmail.com>
2022-03-07 16:04:05 +0300

    Add base auth token middleware test

--- tests/app/tests_app/tests.py
+++ tests/app/tests_app/tests.py
@@ -1,11 +1,36 @@
+from django.contrib.auth import get_user_model
 from django.test import TestCase
 
-from test_app.consumer import MockConsumer
-from test_app.middleware import (
+from tests_app.consumer import MockConsumer
+from tests_app.middleware import (
     TestBaseAuthTokenMiddleware, TestHeaderAuthTokenMiddleware,
     TestCookieAuthTokenMiddleware, TestQueryStringAuthTokenMiddleware,
 )
 
 
 class MiddlewaresTests(TestCase):
-    pass
+
+    @classmethod
+    def setUpClass(cls):
+        User = get_user_model()
+        User.objects.create_user("test", password="test", id=1)
+
+    @classmethod
+    def tearDownClass(cls):
+        pass
+
+    async def test_base_auth_token_middleware(self):
+        app = TestBaseAuthTokenMiddleware(MockConsumer())
+
+        scope = {"headers": [(b"test", b"1")]}
+        updated_scope = await app(scope, None, None)
+        user = updated_scope.get("user")
+        assert user
+        assert not user.is_anonymous
+        assert user.id == 1
+
+        scope = {"headers": [(b"test", b"2")]}
+        updated_scope = await app(scope, None, None)
+        user = updated_scope.get("user")
+        assert user
+        assert user.is_anonymous
