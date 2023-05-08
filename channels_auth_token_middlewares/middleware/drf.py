710086d16a17c11f8e37ed926cd0a4e5c24fd347
Yegor Bitensky <yegorbitensky@gmail.com>
2023-05-09 02:24:07 +0300

    #5 DRF middlewares with query string token

--- channels_auth_token_middlewares/middleware/drf.py
+++ channels_auth_token_middlewares/middleware/drf.py
@@ -22,10 +22,26 @@ from django.apps import apps
 
 from channels.db import database_sync_to_async
 
-from .base import HeaderAuthTokenMiddleware
+from .base import HeaderAuthTokenMiddleware, QueryStringAuthTokenMiddleware
 
 
-class DRFAuthTokenMiddleware(HeaderAuthTokenMiddleware):
+class DRFAuthTokenMiddlewareMixin:
+    """Django REST framework auth token middleware mixin."""
+
+    @database_sync_to_async
+    def get_drf_user_instance(self, token_key):
+        Token = apps.get_model("authtoken", "Token")
+        try:
+            token = Token.objects.select_related("user").get(key=token_key)
+        except Token.DoesNotExist:
+            return None
+        return token.user
+
+
+class DRFAuthTokenMiddleware(
+    HeaderAuthTokenMiddleware,
+    DRFAuthTokenMiddlewareMixin,
+):
     """
     Django REST framework auth token middleware.
 
@@ -36,17 +52,57 @@ class DRFAuthTokenMiddleware(HeaderAuthTokenMiddleware):
     keyword = "Token"
     token_regex = "[0-9a-f]{40}"
 
+    async def get_user_instance(self, token_key):
+        return await self.get_drf_user_instance(token_key)
+
+
+class QueryStringDRFAuthTokenMiddleware(
+    QueryStringAuthTokenMiddleware,
+    DRFAuthTokenMiddlewareMixin,
+):
+    """Django REST framework auth token middleware with query string token."""
+
+    query_param = "authorization"
+
+    async def get_user_instance(self, token_key):
+        return await self.get_drf_user_instance(token_key)
+
+
+def DRFAuthTokenMiddlewareStack(inner):
+    return DRFAuthTokenMiddleware(QueryStringDRFAuthTokenMiddleware(inner))
+
+
+class SimpleJWTAuthTokenMiddlewareMixin:
+    """Simple JWT auth token middleware mixin."""
+
+    _auth = None
+    _exceptions = None
+
+    def _setup(self):
+        from rest_framework_simplejwt.authentication import JWTAuthentication
+        from rest_framework_simplejwt.exceptions import (
+            AuthenticationFailed, InvalidToken, TokenError
+        )
+
+        self._auth = JWTAuthentication()
+        self._exceptions = (AuthenticationFailed, InvalidToken, TokenError)
+
     @database_sync_to_async
-    def get_user_instance(self, token_key):
-        Token = apps.get_model("authtoken", "Token")
+    def get_jwt_user_instance(self, token_key):
+        if self._auth is None or self._exceptions is None:
+            raise RuntimeError("_setup method has to be called before.")
+
         try:
-            token = Token.objects.select_related("user").get(key=token_key)
-        except Token.DoesNotExist:
+            validated_token = self._auth.get_validated_token(token_key)
+            return self._auth.get_user(validated_token)
+        except self._exceptions:
             return None
-        return token.user
 
 
-class SimpleJWTAuthTokenMiddleware(HeaderAuthTokenMiddleware):
+class SimpleJWTAuthTokenMiddleware(
+    HeaderAuthTokenMiddleware,
+    SimpleJWTAuthTokenMiddlewareMixin,
+):
     """
     Simple JWT auth token middleware.
 
@@ -57,20 +113,29 @@ class SimpleJWTAuthTokenMiddleware(HeaderAuthTokenMiddleware):
     keyword = "Bearer"
 
     def __init__(self, *args, **kwargs):
-        from rest_framework_simplejwt.authentication import JWTAuthentication
-        from rest_framework_simplejwt.exceptions import (
-            AuthenticationFailed, InvalidToken, TokenError
-        )
-
-        self._auth = JWTAuthentication()
-        self._exceptions = (AuthenticationFailed, InvalidToken, TokenError)
-
+        self._setup()
         return super().__init__(*args, **kwargs)
 
-    @database_sync_to_async
-    def get_user_instance(self, token_key):
-        try:
-            validated_token = self._auth.get_validated_token(token_key)
-            return self._auth.get_user(validated_token)
-        except self._exceptions:
-            return None
+    async def get_user_instance(self, token_key):
+        return await self.get_jwt_user_instance(token_key)
+
+
+class QueryStringSimpleJWTAuthTokenMiddleware(
+    QueryStringAuthTokenMiddleware,
+    SimpleJWTAuthTokenMiddlewareMixin,
+):
+    """Simple JWT auth token middleware with query string token."""
+
+    query_param = "authorization"
+
+    def __init__(self, *args, **kwargs):
+        self._setup()
+        return super().__init__(*args, **kwargs)
+
+    async def get_user_instance(self, token_key):
+        return await self.get_jwt_user_instance(token_key)
+
+
+def SimpleJWTAuthTokenMiddlewareStack(inner):
+    return SimpleJWTAuthTokenMiddleware(
+        QueryStringSimpleJWTAuthTokenMiddleware(inner))
