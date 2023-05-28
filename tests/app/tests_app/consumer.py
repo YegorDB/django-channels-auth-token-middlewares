382e4db118bf6ea92cde400787ed8ef2ae323483
Yegor Bitensky <yegorbitensky@gmail.com>
2023-05-28 22:04:57 +0300

    Communicator middlewares tests #4

--- tests/app/tests_app/consumer.py
+++ tests/app/tests_app/consumer.py
@@ -1,3 +1,14 @@
+from channels.generic.http import AsyncHttpConsumer
+
+
 class MockConsumer:
     async def __call__(self, scope, receive, send):
         return scope
+
+
+class TestHttpConsumer(AsyncHttpConsumer):
+    async def handle(self, body):
+        user = self.scope["user"]
+        user_id = None if user.is_anonymous else user.id
+
+        await self.send_response(200, str(user_id).encode())
